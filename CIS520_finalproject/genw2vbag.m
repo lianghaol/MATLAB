a6db017364cbd7edd4e8dbcfaded27b53a72c186
Zack <zhaozeyu@seas.upenn.edu>
2018-11-18 18:20:22 -0500

    cleaning

--- CIS520_finalproject/genw2vbag.m
+++
@@ -1,9 +0,0 @@
-
-new_X_train_bag = zeros(18092, 530);
-
-[i,j,s] = find(X_train_bag);
-
-for k = 1:length(i)
-    new_vec_j = w2v(j(k)) + 1;
-    new_X_train_bag(i(k), new_vec_j) = new_X_train_bag(i(k), new_vec_j) + s(k);
-end
--- CIS520_finalproject/knn.m
+++
@@ -1,5 +0,0 @@
- 
-function [P] = knn(X_train_part, Y_train_part, X_test_part)   
-    Mdl = fitcknn(X_train_part,Y_train_part, 'NumNeighbors',20);
-    [~, P, ~] = Mdl.predict(X_test_part);
-end
